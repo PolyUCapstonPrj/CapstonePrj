3bbfef5f2e86e48e1902b261060c0565328ae64d
Chenjun Yan <25110291g@connect.polyu.hk>
2025-12-23 23:29:08 +0800

    Revert "Merge branch 'main' into main"
    This reverts commit 16c433a803625895a95caa8d9023e044892ee2a5, reversing
    changes made to 05d112e1f6778aeabdefafad79e9bd8665e29f30.

--- Source/CapstonePrj/Private/Player/MainPlayer.cpp
+++ Source/CapstonePrj/Private/Player/MainPlayer.cpp
@@ -6,15 +6,14 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
-#include "GameFramework/CharacterMovementComponent.h"
-#include "AbilitySystemComponent.h"
-#include "Abilities/GameplayAbility.h"
+#include  "GameFramework/CharacterMovementComponent.h"
 
 AMainPlayer::AMainPlayer()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
 	PlayerCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
@@ -39,18 +38,6 @@ void AMainPlayer::BeginPlay()
 			}
 		}
 	}
-	
-	// Grant base abilities
-	if (AbilitySystemComponent && HasAuthority())
-	{
-		for (TSubclassOf<UGameplayAbility>& AbilityClass : BaseAbilities)
-		{
-			if (AbilityClass)
-			{
-				AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(AbilityClass, 1, INDEX_NONE, this));
-			}
-		}
-	}
 }
 
 void AMainPlayer::Move(const FInputActionValue& Value)
@@ -72,18 +59,8 @@ void AMainPlayer::Look(const FInputActionValue& Value)
 	FVector2D LookVector = Value.Get<FVector2D>();
 	if (Controller)
 	{
-		// Apply horizontal sensitivity
-		float YawInput = LookVector.X * LookSensitivityX;
-		AddControllerYawInput(YawInput);
-
-		// Apply vertical sensitivity and invert option
-		float PitchInput = LookVector.Y * LookSensitivityY;
-		if (bInvertYAxis)
-		{
-			PitchInput *= -1.0f;
-		}
-		
-		AddControllerPitchInput(PitchInput);
+		AddControllerYawInput(LookVector.X);
+		AddControllerPitchInput(LookVector.Y);
 	}
 }
 
